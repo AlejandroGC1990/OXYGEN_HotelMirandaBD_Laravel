de9c51e8960ce55e0678845e9edf91ce0b08ede0
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-11-07 11:20:49 +0100

    primer commit

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Activity;
+use Illuminate\Support\Facades\Auth;
+
+class ActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view("activities.index", ['activities' => Auth::user() -> activities() -> get()]);                                                   
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("activities.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'type' => ['required', 'string'],
+            'datetime' => ['required', 'string'],
+            'notes' => ['required', 'string'],
+        ]);
+
+        Activity::create([
+            'type' => $request->input('type'),
+            'user_id' => $request->user()->id,
+            'datetime' => $request->input('datetime'),
+            'notes' => $request->input('notes'),
+        ]);
+
+        return redirect(route("activities.index"));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view("activities.show");
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view("activities.edit");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/activities/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="{{route('activities.store')}}">
+        @csrf
+        @method("post")
+        <select name="type">
+            <option value="surf">Surf</option>
+            <option value="windsurf">windsurf</option>
+            <option value="kayak">kayak</option>
+            <option value="atv">atv</option>
+            <option value="hot air ballon">hot air ballon</option>
+        </select>
+        <input name="datetime" type="date" placeholder="fecha"/>
+        <input name="notes" type="text" placeholder="Note"/>
+        <button type="submit">AAAAAAAAAAAAAAA</button>
+    </form>
+
+</body>
+</html>
